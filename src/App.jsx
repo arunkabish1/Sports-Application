dfffdc460b4c2aeb254ad72d3a3d164cee0cfcfc
arunkabish1 <720721110060@hicet.ac.in>
2024-01-08 00:41:13 +0530

    Hello react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-green-500 text-center mt-8">Hello React</h1>
+    </>
+  )
+}
+
+export default App
