2170127904c66a8f451178e6a8421942aa471c2d
arunkabish1 <720721110060@hicet.ac.in>
2024-01-20 20:14:14 +0530

    modal added for livematches

--- src/pages/match/MatchDisplayAll.tsx
+++ src/pages/match/MatchDisplayAll.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useEventsState } from "../../context/match/context";
 import Scoreboard from "../../assets/scoreboard.gif";
+import Modal from "./Modal";
+
 
 const MatchDisplayAll: React.FC = () => {
   const state = useEventsState();
@@ -23,6 +25,12 @@ console.log("Events:",events);
   const ongoingEvents = events.filter((event: any) => event.isRunning);
   console.log("Running events:", ongoingEvents);
   
+  const modelId = (id: number) => {
+    console.log(id);
+    return <Modal id={id} />;
+  }
+
+
   return (
     <div className="ml-5 mt-5">
       <div className="flex flex-row gap-2 ml-1 mt-3">
@@ -30,10 +38,10 @@ console.log("Events:",events);
         <h1 className=" text-2xl font-bold">Live Score</h1>
       </div>
 
-      <div className="flex gap-8">
+      <div className="flex gap-8 overflow-x-auto">
         {ongoingEvents.map((event: any) => (
           <div key={event.id} className="p-2">
-            <div className="w-96 p-4 bg-[#c7e3e2] border border-gray-400 rounded-lg shadow-md text-grey-100 hover:bg-gray-300 cursor-pointer">
+            <div className="w-96 p-4 bg-[#c7e3e2] border border-gray-400 rounded-lg shadow-md text-grey-100 hover:bg-gray-300 cursor-pointer ">
               <div className="flex justify-between">
                 <h5 className="mb-1 text-xl">
                   <span className="mb-2 text-black font-bold">
@@ -46,6 +54,7 @@ console.log("Events:",events);
                   {event.name && event.name.split("at")[0]}
                 </span>
                 <br />
+                <div>{modelId(event.id)}</div>
               </p>
             </div>
           </div>
---
+++ src/pages/match/Modal.tsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import { API_ENDPOINT } from "../../config/constants";
+import { Transition } from "@headlessui/react";
+
+interface EventInfo {
+  id: number;
+  isRunning: boolean;
+  name: string;
+  location: string;
+  startsAt: string;
+  endsAt: string;
+  score: Record<string, string>;
+  teams: { id: number; name: string }[];
+  sportName: string;
+  playingTeam: number;
+  story: string;
+}
+
+const Modal: React.FC<{ id: number }> = ({ id }) => {
+  const [events, setEvents] = useState<EventInfo | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const fetchEventDetails = async () => {
+    const authToken = localStorage.getItem("authToken");
+    try {
+      const response = await fetch(`${API_ENDPOINT}/matches/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch match details");
+      }
+
+      const data: EventInfo = await response.json();
+      setEvents(data);
+    } catch (error) {
+      console.error("Error fetching match details", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEventDetails();
+  }, [id]);
+
+  const renderScore = () => {
+    if (events && events.score) {
+      return (
+        <div>
+          <ul>
+            {Object.entries(events.score).map(([team, points]) => (
+              <li key={team}>
+                {team}: {points}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal} className="text-white rounded-lg cursor-pointer bg-blue-600 px-4 p-1">
+        Read More
+      </button>
+      <Transition show={isOpen} as={React.Fragment}>
+        <div className="fixed inset-0 z-50">
+          <div className="sm:block text-center">
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 transition-opacity">
+                <div className="fixed inset-0 bg-black opacity-75"></div>
+              </div>
+            </Transition.Child>
+
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-[#ffffff] rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:w-1/2 sm:my-10 ">
+                <div className=" px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  {events && (
+                    <div>
+                      <h1 className="text-2xl font-bold text-center p-1">{events.sportName}</h1>
+                      <h2 className="text-xl font-bold text-center p-1">{events.name && events.name.split("at")[0]}</h2>
+                      <h3 className="text-l font-bold text-center">At{events.name && events.name.split("at")[1]}</h3>
+                      <div className="p-1 bg-[#c7e3e2] mb-2 mt-3 border border-gray-400 rounded-lg shadow-md text-grey-100 hover:bg-gray-300 cursor-pointer ">
+                     <div className="flex p-6">
+                     <h3 className=" text-4xl font-bold ">Score:</h3>
+                      <h3 className="text-l ml-5 font-bold">{renderScore()}</h3>
+                     </div>
+                      <p className="p-1 text-zinc-900">Match Location : {events.location}</p>
+                      <p className=" p-1 text-zinc-900">Starting Time : {events.startsAt}</p>
+                      <p className=" p-1 text-zinc-900">Ending Time : {events.endsAt}</p>                      
+                      </div>
+                      
+                      <div className="overflow-y-auto p-1 bg-[#c7e3e2] border border-gray-400 rounded-lg shadow-md text-grey-100 hover:bg-gray-300 cursor-pointer">
+                      <h4 className="text-xl font-bold text-center p-1">Match Summary</h4>
+                      <p className="text-gray-800 font-semibold">{events.story}</p>
+                      </div>
+                      
+                    </div>
+                  )}
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    onClick={closeModal}
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </div>
+      </Transition>
+    </div>
+  );
+};
+
+export default Modal;
+
